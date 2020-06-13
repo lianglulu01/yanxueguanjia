16aaacba4d6050290d6152ed899016206061c57c
xuzheng <xusirwiner@163.com>
2020-06-13 13:48:27 +0800

    2020-06-12圈子

--- pages/dynamic/dynamic_index.js
+++ pages/dynamic/dynamic_index.js
@@ -9,17 +9,37 @@ Page({
     selected1: false,
     length:5,
     list:5,
-    jump:true,
+    jump:false,
     tongming:false,
     last:true,
     dynamicArr:[{length:9}]
   },
-
+  detail:function(e){
+    console.log(e);
+    wx.navigateTo({
+      url: '/pages/activeDetail/activeDetail?id='+e.currentTarget.dataset.id,
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+        var that = this;
+        console.log(options.circel_id);
+        var data = {
+           user_id:wx.getStorageSync('userinfo').id,
+           circel_id:options.circel_id
+        }
+        wx.request({
+          url: 'https://yanxue.qiweibang.com/web/index.php?r=api/circel/circel-detail',
+          data:data,
+          success:function(res){
+               console.log(res)
+               that.setData({
+                   detail:res.data.data
+               })
+          }
+        })
   },
   tongming:function(){
     this.setData({
