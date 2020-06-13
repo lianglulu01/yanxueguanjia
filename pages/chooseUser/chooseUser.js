5340158d853959149b6d926642bb5b9f4c81c9d8
lianglulu01 <844432381@qq.com>
2020-06-13 13:20:18 +0800

    6.13

--- pages/chooseUser/chooseUser.js
+++ pages/chooseUser/chooseUser.js
@@ -4,7 +4,9 @@ Page({
   /* 页面的初始数据 */
   data: {
     userList: [],
-    checked:false,
+    joinUserId: [],
+    joinUserName: [],
+    is_check: "",
   },
 
   /* 生命周期函数--监听页面加载 */
@@ -16,37 +18,54 @@ Page({
       method: 'GET',
       dataType: 'json',
       responseType: 'text',
-      success:res=>{
+      success: res => {
         console.log(res.data.data)
         this.setData({
           userList: res.data.data.list
         })
       },
-      fail: function(res) {},
-      complete: function(res) {},
+      fail: function (res) { },
+      complete: function (res) { },
     })
-    
-  },
-  isChoose(){
-    if (!this.data.checked){
-      this.setData({
-        checked:true
-      })
-      // console.log(this.data.checked)
-    }else{
-      this.setData({
-        checked: false
-      })
-      // console.log(this.data.checked)
-    }
   },
 
-  backPay:function(){
-    if (this.data.checked){
-       wx.navigateBack({
-         delta: 1,
-       })
-    }
+  // 选择参加活动用户
+  checkbox: function (e) {
+    // console.log(e)
+    console.log(e.currentTarget.dataset.id)
+    var index = e.currentTarget.dataset.index;//获取当前点击的下标
+    var userList = this.data.userList;//选项集合
+    userList[index].is_check = !userList[index].is_check;//改变当前选中的checked值
+    var joinUserId = this.data.joinUserId.push(e.currentTarget.dataset.id)
+    // console.log(this.data.joinUserId)
+    this.setData({
+      userList: userList,
+      joinUserId: this.data.joinUserId
+    });
+  },
+  checkboxChange: function (e) {
+    console.log(e)
+    this.setData({
+      checkValue: e.detail.value,
+    });
+  },
+  // 确定选择后 返回付款页
+  confirm: function () {
+    // console.log(this.data.checkValue)//所有选中的项的value
+    console.log(this.data)
+    this.setData({
+      joinUserId: this.data.joinUserId,
+      joinUserName: this.data.checkValue,
+    })
+    let pages = getCurrentPages();
+    let prevPage = pages[pages.length - 2];  //获取上一个页面
+    prevPage.setData({
+      joinUserId: this.data.joinUserId,
+      joinUserName: this.data.checkValue,
+    })
+    wx.navigateBack({
+      delta: 1
+    })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
